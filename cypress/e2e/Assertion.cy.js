78045414894a4f8e0855c94e1161b2ed3138116e
Deen-2601 <yussufnurudeen@gmail.com>
2023-01-17 15:10:50 +0100

    init

--- cypress/e2e/Assertion.cy.js
+++ cypress/e2e/Assertion.cy.js
@@ -21,6 +21,34 @@ describe('Assertions demo', () =>{
 
 
 
+    it('Explicit Assertions', () =>{
+
+        cy.visit("https://www.saucedemo.com/")  //visit the url
+        
+        cy.get("#user-name").type("standard_user") //type in the username
+    
+        cy.get("#password").type("secret_sauce") //type in the password
+        
+        cy.get("[name='login-button']").click() //click the Login button
+
+        let expName = 'xyz';
+        
+        cy.get("inventory_item_name").then( (x)=>{
+
+                let actName=x.text()
+                
+                //BDD style
+                expect(actName).to.equal(expName)
+                expect(actName).to.not.equal(expName)
+
+                //TDD style
+                assert.equal(actName,expName)
+                assert.notEqual(actName,expName)
+
+
+
+        })
+    })
 
 
 })
