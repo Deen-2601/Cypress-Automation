8be3636ab9e6c4ed17248fb915aa95b56d9326f8
Deen-2601 <yussufnurudeen@gmail.com>
2023-01-27 14:41:13 +0100

    init

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -8,6 +8,9 @@ describe('handle dropdowns', () =>{
         cy.get('#zcf_address_country').select('Nigeria')
         .should('have.value','Nigeria')
 
+        cy.get('#zcf_users').select('Selling on marketplaces')
+        .should('have.value','Selling on marketplaces')
+
 
 
     })
