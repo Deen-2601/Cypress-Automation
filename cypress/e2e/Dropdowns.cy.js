35ce5436b2a7b1b23d6d059bc7872b5cf7e8f287
Deen-2601 <yussufnurudeen@gmail.com>
2023-01-29 22:39:56 +0100

    init

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -1,11 +1,25 @@
 
 describe('handle dropdowns', () =>{
 
-    it('Dropdown with select', () =>{
+    itskip('Dropdown with select', () =>{
 
         cy.visit('https://www.zoho.com/commerce/free-demo.html')
 
         cy.get('#zcf_address_country').select('Nigeria')
+        .should('have.value','Nigeria')
+
+        cy.get('#zcf_users').select('Selling on marketplaces')
+        .should('have.value','Selling on marketplaces')
+
+
+
+    })
+
+    it('Dropdown without select', () =>{
+
+        cy.visit("https://www.dummytitcket.com/dummy-ticket-for-visa-application/")
+
+        cy.get('#select2-billing').select('Nigeria')
         .should('have.value','Nigeria')
 
         cy.get('#zcf_users').select('Selling on marketplaces')
